2d9d455e299a61a04e69459c59ada681aeec4217
Sawal Timsina <t.sawal231@gmail.com>
2025-03-24 18:53:18 +0545

    mui component preview

---
+++ src/components/MuiComponentSamples/MuiComponentSamples.tsx
@@ -0,0 +1,44 @@
+import { Box, Button, Grid2, Typography } from "@mui/material"
+import componentSamples from "./Samples"
+
+const MuiComponentSamples = () => {
+  return (
+    <Box>
+      <Typography variant="h4" gutterBottom>
+        Material-UI Components
+      </Typography>
+      {componentSamples.map(({ id, title, component, docs }) => (
+        <div key={id} id={id}>
+          <Grid2 container justifyContent="space-between" alignItems="center" padding="8px">
+            <Typography variant="h5" gutterBottom>
+              {title}
+            </Typography>
+            <Button
+              variant="outlined"
+              color="secondary"
+              size="small"
+              href={docs}
+              target="_blank"
+              rel="noreferrer"
+            >
+              Docs
+            </Button>
+          </Grid2>
+          <Box
+            sx={{
+              mb: 10,
+              width: 1,
+              maxWidth: 1000,
+              pl: 1,
+              m: "auto",
+            }}
+          >
+            {component}
+          </Box>
+        </div>
+      ))}
+    </Box>
+  )
+}
+
+export default MuiComponentSamples
